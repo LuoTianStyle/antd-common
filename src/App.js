5e73665b7da6b5565252ccad6fdcebd0f919aa10
tian <tianluoluo666@gmail.com>
2020-04-14 18:18:35 +0800

    添加文件

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { DatePicker } from 'antd';
-import moment from 'dayjs';
-const { RangePicker } = DatePicker;
-const dateFormat = 'YYYY/MM/DD';
+import React from 'react'
+import { DatePicker } from 'antd'
+import moment from 'dayjs'
+const { RangePicker } = DatePicker
+const dateFormat = 'YYYY/MM/DD'
 const App = () => (
   <div>
     <RangePicker
-      defaultValue={[moment('2016/01/01', dateFormat), moment('2015/01/01', dateFormat)]}
+      defaultValue={[
+        moment('2016/01/01', dateFormat),
+        moment('2015/01/01', dateFormat),
+      ]}
       format={dateFormat}
     />
   </div>
-);
-export default App;
-
-
-
-
+)
+export default App
