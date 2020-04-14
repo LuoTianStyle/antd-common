f9376bdbc8c231b2da088aff3fadd880adc2cb7e
tian <tianluoluo666@gmail.com>
2020-04-14 15:16:59 +0800

    antd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import { DatePicker } from 'antd';
+import moment from 'dayjs';
+const { RangePicker } = DatePicker;
+const dateFormat = 'YYYY/MM/DD';
+const App = () => (
+  <div>
+    <RangePicker
+      defaultValue={[moment('2016/01/01', dateFormat), moment('2015/01/01', dateFormat)]}
+      format={dateFormat}
+    />
+  </div>
+);
 export default App;
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/es/locale/zh_CN';
+import 'moment/locale/zh-cn';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+if (module.hot) {
+  module.hot.accept();
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
